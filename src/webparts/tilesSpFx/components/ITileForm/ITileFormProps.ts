0f0a18ad73aa60a92d458625bdaf10599fe7251d
Maya-Mostafa <mayamostafa@gmail.com>
2020-12-07 14:31:37 -0500

    separating dialog and form components

---
+++ src/webparts/tilesSpFx/components/ITileForm/ITileFormProps.ts
@@ -0,0 +1,14 @@
+export interface ITileFormProps{
+    formField:any;
+    colorField:any;
+    iconField:any;
+    openNewWin:any;
+
+    onChangeFormField: any;
+    onChangeColorField: any;
+    onChangeIconField: any;
+    onChangeOpenNewWin: any;
+
+    errorMsgTitle : string;
+    errorMsgLink : string;
+}
