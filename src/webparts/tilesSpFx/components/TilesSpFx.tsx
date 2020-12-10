1e0952a48f2de665bb37344ecc295d8750e0c3c8
Mostafa <mai.mostafa@peelsb.com>
2020-12-10 11:03:35 -0500

    using formfield one state object instead of separate ones + moving dialog component here +implementing delete feature

--- src/webparts/tilesSpFx/components/TilesSpFx.tsx
+++ src/webparts/tilesSpFx/components/TilesSpFx.tsx
@@ -3,15 +3,15 @@ import styles from './TilesSPFx.module.scss';
 import { ITilesSPFxProps } from './ITilesSPFxProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
-import {Label} from '@fluentui/react';
+import {Label, Dialog, DialogType, DialogFooter, PrimaryButton, DefaultButton} from '@fluentui/react';
 import {getTilesData, updateIcon} from '../Services/DataRequests';
 import ITile from './ITile/ITile';
 import ITileControls from './ITileControls/ITileControls';
+import ITileForm from './ITileForm/ITileForm';
 
 import {addTile, deleteTile, updateTile, getTile} from '../Services/DataRequests';
 
 import { useBoolean } from '@uifabric/react-hooks';
-import {IDropdownOption} from '@fluentui/react';
 
 export default function TilesSPFx (props: ITilesSPFxProps) {
 
@@ -19,38 +19,50 @@ export default function TilesSPFx (props: ITilesSPFxProps) {
   
     const [formField, setFormField] = React.useState({
       titleField: "",
-      linkField: ""
+      linkField: "",
+      colorField: {key: "", text: "", data: {icon: ""}},
+      iconField : {key : "" , text:"", data: {icon: ""}},
+      openNewWin: true,
+      idField : ""
     });
     const onChangeFormField = React.useCallback(
-      (event: any, newValue?: string) => {      
+      (event: any, newValue?: any) => {      
         setFormField({
           ...formField,
-          [event.target.name]: newValue || ''
+          [event.target.id]: typeof newValue === "boolean" ? !!newValue : newValue || ''
         });
       },
       [formField],
     );
+    const resetFields = () =>{
+      setFormField({
+        titleField: "",
+        linkField: "",
+        colorField: {key: "", text: "", data: {icon: ""}},
+        iconField : {key : "" , text:"", data: {icon: ""}},
+        openNewWin: true,
+        idField : ""
+      });
+    };
     
-    const [colorField, setColorField] = React.useState<IDropdownOption>();
-    const colorFieldBase = colorField;
-    const onChangeColorField = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
-      setColorField(item);
-      console.log("color item", item);
-    };
-  
-    const [iconField, setIconField] = React.useState<IDropdownOption>();
-    const iconFieldBase = iconField;
-    const onChangeIconField = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
-      setIconField(item);
-      console.log("icon item", item);
-    };
-  
-    const [openNewWin, { toggle: toggleOpenNewWin }] = useBoolean(true);
-    const onChangeOpenNewWin = (ev: React.MouseEvent<HTMLElement>, checked: boolean) =>{
-      toggleOpenNewWin();
-    };
-
+    const [isNewDialog, setIsNewDialog] = React.useState(true);
     const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true);
+    const handleToggleHideDialog = () => {
+      resetFields();
+      setIsNewDialog(true); 
+      toggleHideDialog();
+    };
+    const dialogContentProps = {
+      type: DialogType.close,
+      title: isNewDialog ? "Add a new Tile" : "Update Tile"
+    }; 
+
+    const deleteDialogContentProps = {
+      type: DialogType.close,
+      title: "Delete Tile"
+    }; 
+    const [hideDeleteDialog, {toggle: toggleHideDeleteDialog}] = useBoolean(true);
+
     const [errorMsgTitle, setErrorMsgTitle] = React.useState('');
     const [errorMsgLink, setErrorMsgLink] = React.useState('');
     
@@ -65,16 +77,17 @@ export default function TilesSPFx (props: ITilesSPFxProps) {
       });      
     },[tilesData.length]);
 
-    const resetFields = () =>{
-      setFormField({
-        titleField:"",
-        linkField: ""
-      });
-      setColorField(colorFieldBase);
-      setIconField(iconFieldBase);
+    const handleIconSave = (itemId: any)=>{
+      return (tIconName: string)=>{
+        updateIcon(props.context, itemId, tIconName).then(()=>{
+          getTilesData(props.context, props.orderBy).then((results)=>{
+            setTilesData(results);
+          });
+        });
+      };
     };
 
-    const addTileItem = () =>{      
+    const addTileItem = () =>{         
       setErrorMsgTitle("");
       setErrorMsgLink("");
       if (formField.titleField == "" && formField.linkField == ""){
@@ -94,9 +107,9 @@ export default function TilesSPFx (props: ITilesSPFxProps) {
         const tileInfo : any = {
           Title: formField.titleField,
           Link: formField.linkField,
-          Color: colorField ? colorField.text : "Blue",
-          Icon: iconField ? iconField.data.icon : "",
-          OpenNewWin: openNewWin
+          Color: formField.colorField.text,
+          Icon: formField.iconField.data.icon,
+          OpenNewWin: formField.openNewWin
         };
         addTile(props.context, tileInfo).then(()=>{
           getTilesData(props.context, props.orderBy).then((results)=>{
@@ -106,44 +119,56 @@ export default function TilesSPFx (props: ITilesSPFxProps) {
       }
     };
 
-    const openDialog = () => {
-      //resetFields();
-      toggleHideDialog();
-    };
-
-    const handleIconSave = (itemId: any)=>{
-      return (tIconName: string)=>{
-        updateIcon(props.context, itemId, tIconName).then(()=>{
-          getTilesData(props.context, props.orderBy).then((results)=>{
-            setTilesData(results);
-          });
-        });
-      };
-    };
-
     const handleDelete = (itemId: any)=>{
-      return ()=>{
-        deleteTile(props.context, itemId).then(()=>{
-          getTilesData(props.context, props.orderBy).then((results)=>{
-            setTilesData(results);
-          });
+      return () =>{
+        setFormField({
+          ...formField,
+          idField : itemId
         });
+        toggleHideDeleteDialog();
       };
+    };
+    const deleteTileItem = () =>{
+      deleteTile(props.context, formField.idField).then(()=>{
+        getTilesData(props.context, props.orderBy).then((results)=>{
+          setTilesData(results);          
+        });
+      }).then(()=>{
+        toggleHideDeleteDialog();
+      });
     };
 
     const handleEdit = (itemId: any)=>{
       return ()=>{
         getTile(props.context, itemId).then((result :any)=>{
+          setIsNewDialog(false);
           toggleHideDialog();
           setFormField({
             titleField: result.Title,
-            linkField: result.Link
+            linkField: result.Link,
+            colorField : {key: result.Color.toLowerCase(), text: result.Color, data: {icon: "CircleFill"}},
+            iconField: {key: result.IconName, text: result.IconName, data: {icon: result.IconName}},
+            openNewWin: result.OpenNewWin,
+            idField : result.Id
           });           
-          setColorField({key: result.Color.toLowerCase(), text: result.Color, data: {icon: "CircleFill"}});
-          setIconField({key: result.IconName, text: result.IconName, data: {icon: result.IconName}});
         });
       };
     };
+    const updateTileItem = () =>{
+      updateTile(props.context, formField.idField, {
+        Title: formField.titleField,
+        Link: formField.linkField,
+        Color: formField.colorField.text,
+        IconName: formField.iconField.data.icon,
+        OpenInNewWindow: formField.openNewWin
+      }).then(()=>{
+        getTilesData(props.context, props.orderBy).then((results)=>{
+          setTilesData(results);
+          toggleHideDialog();
+        });
+      });
+    };
+    
 
     return (
       <div className={styles.tilesSPFx}>
@@ -171,14 +196,36 @@ export default function TilesSPFx (props: ITilesSPFxProps) {
         </div>
 
         <ITileControls context={props.context} 
-          formField = {formField} onChangeFormField={onChangeFormField}
-          colorField = {colorField} onChangeColorField = {onChangeColorField}
-          iconField = {iconField} onChangeIconField={onChangeIconField}
-          openNewWin= {openNewWin} onChangeOpenNewWin={onChangeOpenNewWin}
-          hideDialog = {hideDialog} toggleHideDialog={toggleHideDialog}
-          addTileItem = {addTileItem} errorMsgTitle={errorMsgTitle} errorMsgLink={errorMsgLink}
+          toggleHideDialog={handleToggleHideDialog} 
           handleEditChange={handleEditChange} 
         />
+
+        <Dialog
+            hidden={hideDialog}
+            onDismiss={toggleHideDialog} isBlocking={true}
+            dialogContentProps={dialogContentProps}>
+            <ITileForm 
+              formField={formField} onChangeFormField={onChangeFormField}
+              errorMsgTitle={errorMsgTitle} errorMsgLink={errorMsgLink}/>
+            <DialogFooter>
+                {isNewDialog 
+                    ? <PrimaryButton onClick={addTileItem} text="Save" />
+                    : <PrimaryButton onClick={updateTileItem} text="Update" />
+                }
+                <DefaultButton onClick={toggleHideDialog} text="Cancel" />
+            </DialogFooter>
+        </Dialog>
+
+        <Dialog
+            hidden={hideDeleteDialog}
+            onDismiss={toggleHideDeleteDialog} isBlocking={true}
+            dialogContentProps={deleteDialogContentProps}>
+            <p>Are you sure you want to delete this tile? </p>
+            <DialogFooter>
+                <PrimaryButton onClick={deleteTileItem} text="Yes" />
+                <DefaultButton onClick={toggleHideDeleteDialog} text="No" />
+            </DialogFooter>
+        </Dialog>
 
       </div>
     );
