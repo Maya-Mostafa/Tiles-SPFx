f0f78dfc2f8c44776952895dec99f287501eaa18
Mostafa <mai.mostafa@peelsb.com>
2020-12-10 11:01:19 -0500

    removing dialog from component

--- src/webparts/tilesSpFx/components/ITileControls/ITileControls.tsx
+++ src/webparts/tilesSpFx/components/ITileControls/ITileControls.tsx
@@ -5,8 +5,6 @@ import {ITileControlsProps} from './ITileControlsProps';
 import {CommandBarButton, IIconProps, Toggle} from '@fluentui/react';
 import { initializeIcons } from '@uifabric/icons';
 
-import ITileForm from '../ITileForm/ITileForm';
-import IDialog from '../IDialog/IDialog';
 import {isUserManage} from '../../Services/DataRequests';
 
 export default function ITileControls (props: ITileControlsProps) {
@@ -16,20 +14,10 @@ export default function ITileControls (props: ITileControlsProps) {
 
   return (
     <div className={styles.tileControls}>
-        
       <CommandBarButton iconProps={addIcon} text="Add Tile" onClick={props.toggleHideDialog} />
       {isUserManage(props.context) &&
         <Toggle className={styles.tilesEditToggle} label="Edit Tiles" inlineLabel onChange={props.handleEditChange} />
       }
-
-      <IDialog hideDialog={props.hideDialog} toggleHideDialog={props.toggleHideDialog} addTileItem={props.addTileItem}>
-        <ITileForm formField={props.formField} colorField={props.colorField} iconField={props.iconField} openNewWin={props.openNewWin}
-          onChangeFormField={props.onChangeFormField} onChangeOpenNewWin={props.onChangeOpenNewWin}
-          onChangeColorField={props.onChangeColorField} onChangeIconField={props.onChangeIconField} 
-          errorMsgTitle={props.errorMsgTitle} errorMsgLink={props.errorMsgLink}
-        />
-      </IDialog>
-
     </div>
   );
 }
