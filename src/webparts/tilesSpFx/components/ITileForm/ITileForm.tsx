12c5cd6af8e86f5752ae0e39a45a91fc4bff15a3
Maya-Mostafa <mayamostafa@gmail.com>
2021-01-05 19:53:04 -0500

    adding the icon,file,img picker fnc and styles

--- src/webparts/tilesSpFx/components/ITileForm/ITileForm.tsx
+++ src/webparts/tilesSpFx/components/ITileForm/ITileForm.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import {ITileFormProps} from './ITileFormProps';
 import styles from '../TilesSPFx.module.scss';
 
-import {Stack, TextField, Dropdown,IDropdownOption, Toggle, VirtualizedComboBox, IComboBoxOption, ChoiceGroup, IChoiceGroupOption, PrimaryButton} from '@fluentui/react';
+import {Stack, TextField, Dropdown,IDropdownOption, Toggle, VirtualizedComboBox, IComboBoxOption, ChoiceGroup, IChoiceGroupOption} from '@fluentui/react';
 import {getIconNames, getColors} from '../../Services/Styling';
 
 import { initializeIcons } from '@uifabric/icons';
 import {Icon} from '@fluentui/react/lib/Icon';
-import { Label } from 'office-ui-fabric-react';
 
 import { FilePicker, IFilePickerResult } from '@pnp/spfx-controls-react/lib/FilePicker';
 import { IconPicker } from '@pnp/spfx-controls-react/lib/IconPicker';
+import { FileTypeIcon, ApplicationType, IconType } from "@pnp/spfx-controls-react/lib/FileTypeIcon";
 
 export default function ITileForm (props: ITileFormProps) {
 
@@ -65,7 +65,7 @@ export default function ITileForm (props: ITileFormProps) {
         </div>
       );
     };
-  const onRenderIconTitle = (options: IDropdownOption[]): JSX.Element => {
+    const onRenderIconTitle = (options: IDropdownOption[]): JSX.Element => {
       const option = options[0];
       return (
         <div>
@@ -75,20 +75,16 @@ export default function ITileForm (props: ITileFormProps) {
           <span>{option.text}</span>
         </div>
       );
-  };
+    };
 
   const radioOptions: IChoiceGroupOption[] = [
-    // { key: 'AutoSelected', text: 'Auto-selected' },
-    { key: 'Icon', text: 'Icon', name:'globe' },
-    { key: 'CustomImage', text: 'Custom Image', name:'picturefill' },
+    { key: 'Auto', text: 'Auto-selected' },
+    { key: 'Icon', text: 'Icon' },
+    { key: 'Image', text: 'Custom Image'},
   ];
-  const [selectedKey, setSelectedKey] = React.useState<string>('Icon');
-  const [selectedThumbnail, setSelectedThumbnail] = React.useState<string>('globe');
-  const [selectedImg, setSelectedImg] = React.useState(null);
-  const onRadioChange = React.useCallback((ev: React.SyntheticEvent<HTMLElement>, option: IChoiceGroupOption) => {
-    setSelectedKey(option.key);
-    setSelectedThumbnail(option.name);
-  }, []);
+  
+  const [selectedIcon, setSelectedIcon] = React.useState({auto: IconType.font, font: null, img: null});
+  
 
     return(
         <div className={styles.tileForm}>
@@ -104,12 +100,6 @@ export default function ITileForm (props: ITileFormProps) {
                         options={colorOptions} styles={dropdownStyles}
                         onRenderTitle={onRenderColorTitle} onRenderOption={onRenderColorOption}
                         onChange={props.onChangeFormField} />
-                      {/* <Dropdown
-                        placeholder="Select an icon" id="iconField"
-                        label="Icon" selectedKey={props.formField.iconField ? props.formField.iconField.key : undefined}
-                        options={iconOptions} styles={dropdownStyles}
-                        onRenderTitle={onRenderIconTitle} onRenderOption={onRenderIconOption}
-                        onChange={props.onChangeFormField} /> */}
                         <div className={styles.iconDpdCntnr}>
                           <VirtualizedComboBox id="iconField" className={styles.iconDpd}
                               label="Icon Name" placeholder="Select or search for an icon"
@@ -125,19 +115,35 @@ export default function ITileForm (props: ITileFormProps) {
                     <Toggle id="openNewWin" label="Open in a new window" defaultChecked onText="Yes" offText="No" 
                       checked={props.formField.openNewWin} onChange={props.onChangeFormField} />
                     
-                    <ChoiceGroup selectedKey={selectedKey} options={radioOptions} onChange={onRadioChange} label="Thumbnail" />
-                    <Icon iconName={selectedThumbnail} />
-                    {/* <PrimaryButton text="Change" /> */}
-
-                    <FilePicker context={props.context}
-                      accepts= {[".gif", ".jpg", ".jpeg", ".bmp", ".dib", ".tif", ".tiff", ".ico", ".png", ".jxr", ".svg"]}
-                      buttonIcon="FileImage"
-                      onSave={(filePickerResult: IFilePickerResult) => { setSelectedImg({filePickerResult}); }}
-                    />
-
-                    <IconPicker buttonLabel={'Icon'}
-                      onSave={(iconName: string) => { setSelectedThumbnail(iconName); }} />
-
+                    <ChoiceGroup selectedKey={props.selectedKey} options={radioOptions} onChange={props.onRadioChange} label="Thumbnail" />
+                    {props.selectedKey == "Auto" &&
+                      <div className={styles.panelFileType}>
+                        <FileTypeIcon type={IconType.font} path={props.formField.linkField} />
+                      </div>
+                    }
+                    {props.selectedKey == "Icon" &&
+                      <div>
+                        {selectedIcon.font
+                          ? <Icon className={styles.panelIcon} iconName={selectedIcon.font} />  
+                          : <Icon className={styles.panelIcon} iconName="globe" />  
+                        }
+                        <IconPicker buttonLabel="Change"
+                          onSave={(iconName: string) => { setSelectedIcon({ ...selectedIcon, ['font']: iconName }); }} />
+                      </div>
+                    }
+                    {props.selectedKey == "Image" &&
+                      <div>
+                        {selectedIcon.img  
+                          ? <img className={styles.panelImg} src={selectedIcon.img} height="40px"/>
+                          : <Icon className={styles.panelIcon} iconName="picturefill" />  
+                        }
+                        <FilePicker context={props.context}
+                          accepts= {[".gif", ".jpg", ".jpeg", ".bmp", ".dib", ".tif", ".tiff", ".ico", ".png", ".jxr", ".svg"]}
+                          buttonLabel="Change"
+                          onSave={(filePickerResult: IFilePickerResult) => {setSelectedIcon({ ...selectedIcon, ['img']: filePickerResult.fileAbsoluteUrl }); }}
+                        />
+                      </div>
+                    }
                 </Stack>
             </Stack>
         </div>
